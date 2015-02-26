58a5a9db77140a26b9d69055b840f57e1cb91a86
erikanderson <edawebdesign@gmail.com>
2015-02-25 21:10:16 -0700

    remove obsolete code

--- public/app/templates/home/home-control.js
+++ public/app/templates/home/home-control.js
@@ -16,44 +16,7 @@ app.controller('homeCtrl', function($scope, homeService, authService, earthquake
     })
   }
 
-  // $scope.getDataLastHour(user);
-
-  setInterval(function(){
-  console.log('Injected user: ', user);
-  }, 10000);
-
   setInterval(function(){
   $scope.getDataLastHour(user);
   }, 20000);
-
-  // var user = authService.getUser();
-
-  // $scope.getDataLastHourLoggedIn = function(){
-  //   homeService.getHourlyDataLoggedIn().then(function(res){
-  //     console.log('logged in control res: ', res)
-  //     $scope.headerData = res.metadata;
-  //     if (res.features.length === 0){
-  //       $scope.noQuake = "No earthquakes recorded in the last hour.";
-  //     } else {
-  //       $scope.earthquakes = res.features;
-  //     }
-  //     console.log(res.features.length);
-  //     $scope.updateTime = Date.now();
-  //   })
-  // }
-
-  // if (!userLoggedIn){
-  //   console.log('user is not logged in');
-  //   $scope.getDataLastHour();
-  //   setInterval(function(){
-  //     $scope.getDataLastHour();
-  //   }, 20000);
-  // } else if (userLoggedIn){
-  //   console.log('user is logged in');
-  //   $scope.getDataLastHourLoggedIn();
-  //   setInterval(function(){
-  //     $scope.getDataLastHourLoggedIn();
-  //   }, 20000);
-  // }
-
 })
--- public/app/templates/home/home-service.js
+++ public/app/templates/home/home-service.js
@@ -19,8 +19,6 @@ app.service('homeService', function($http, $q, authService){
     return dist
   }  
 
-  // var user = authService.getUser();
-
   this.getHourlyData = function(user){
     if (!user) {
       console.log('NO USER');
@@ -57,30 +55,6 @@ app.service('homeService', function($http, $q, authService){
     }
     return deferred.promise;
   }
-
-  // this.getHourlyDataLoggedIn = function(){
-  //   var userCalc = authService.getUser(); 
-  //   var latitude1 = userCalc.latitude;
-  //   var longitude1 = userCalc.longitude;
-  //   var deferred = $q.defer();
-  //   $http({
-  //     method: 'GET',
-  //     url: '/api/data'
-  //   }).then(function(res){
-  //     // console.log('//////// userCalc: ', userCalc);
-  //     // console.log('******* api data: ', res);
-  //     for (var i = 0; i < res.data.features.length; i++) {
-  //       var latitude2 = res.data.features[i].geometry.coordinates[1];
-  //       var longitude2 = res.data.features[i].geometry.coordinates[0];
-  //       // console.log('in for loop: ', latitude2, longitude2);
-  //       var distanceToUser = distanceCalc(latitude1, longitude1, latitude2, longitude2);
-  //       res.data.features[i].distanceToUser = distanceToUser;
-  //       // console.log(res.data.features[i].distanceToUser);
-  //     }
-  //     deferred.resolve(res.data);
-  //   })
-  //   return deferred.promise;
-  // }
 })
 
 
