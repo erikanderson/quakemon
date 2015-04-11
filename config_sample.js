2c2814ffd87e5d6ac2b5c70ef60ca2b0de085739
Erik Anderson <edawebdesign@gmail.com>
2015-04-11 12:10:51 -0600

    Update config_sample.js

--- config_sample.js
+++ config_sample.js
@@ -1,7 +1,11 @@
 module.exports = {
-  'port': process.env.PORT || 8080,
-  'database': dbpath,
-  'session_secret': session_secret,
-  'google_client_id': google_client_id,
-  'google_client_secret': google_clint_secret
+	'port': process.env.PORT || 8080,
+	'database': 'database',
+	'session_secret': 'session_secret',
+	'google_client_id': 'google_client_id',
+	'google_client_secret': 'google_client_secret',
+	'callback': 'http://localhost:8080/auth/google/callback',
+	'mandrill_key' : 'mandrill_key',
+	'twilio_sid': 'twilio_sid',
+	'twilio_auth_token' : 'twilio_auth_token'
 }
